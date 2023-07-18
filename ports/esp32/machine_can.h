3fa4ef01fb0229adc768af095dd0e6ef18642e79
zzfjean <47771147+zzfjean@users.noreply.github.com>
2023-07-18 08:16:02 +0800

    Uploaded CAN port
    Latest code from https://github.com/IhorNehrutsa/micropython/tree/110790f9b454ce02c3532bae6542d1c997a8b280/ports/esp32
    
    
    Signed-off-by: zzfjean <47771147+zzfjean@users.noreply.github.com>

--- ports/esp32/machine_can.h
+++ ports/esp32/machine_can.h
@@ -3,7 +3,7 @@
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2014 Damien P. George
+ * Copyright (c) 2019 Musumeci Salvatore
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -27,111 +27,51 @@
 #define MICROPY_INCLUDED_ESP32_CAN_H
 
 #include "modmachine.h"
+#include "freertos/task.h"
 
 #include "py/obj.h"
 
+#if MICROPY_HW_ENABLE_CAN
+
 #define DEVICE_NAME "CAN"
 
-#define CAN_BAUDRATE_25k 25
-#define CAN_BAUDRATE_50k 50
-#define CAN_BAUDRATE_100k 100
-#define CAN_BAUDRATE_125k 125
-#define CAN_BAUDRATE_250k 250
-#define CAN_BAUDRATE_500k 500
-#define CAN_BAUDRATE_800k 800
-#define CAN_BAUDRATE_1M 1000
+typedef enum _filter_mode_t {
+    FILTER_RAW_SINGLE = 0,
+    FILTER_RAW_DUAL,
+    FILTER_ADDRESS
+} filter_mode_t;
 
-
-typedef struct _machine_can_config_t {
-    const can_timing_config_t *timing;
-    const can_filter_config_t *filter;
-    const can_general_config_t *general;
-    uint16_t baudrate;
+typedef struct _esp32_can_config_t {
+    twai_timing_config_t timing;
+    twai_filter_config_t filter;
+    twai_general_config_t general;
+    uint32_t baudrate; // bit/s
     bool initialized;
-} machine_can_config_t;
+} esp32_can_config_t;
 
-typedef struct _machine_can_obj_t {
+typedef struct _esp32_can_obj_t {
     mp_obj_base_t base;
-    machine_can_config_t *config;
-} machine_can_obj_t;
+    esp32_can_config_t *config;
+    mp_obj_t rxcallback;
+    TaskHandle_t irq_handler;
+    byte rx_state;
+    bool extframe : 1;
+    bool loopback : 1;
+    byte last_tx_success : 1;
+    byte bus_recovery_success : 1;
+    uint16_t num_error_warning; //FIXME: populate this value somewhere
+    uint16_t num_error_passive;
+    uint16_t num_bus_off;
+} esp32_can_obj_t;
 
-//Internal functions
-STATIC can_state_t _machine_hw_can_get_state();
+typedef enum _rx_state_t {
+    RX_STATE_FIFO_EMPTY = 0,
+    RX_STATE_MESSAGE_PENDING,
+    RX_STATE_FIFO_FULL,
+    RX_STATE_FIFO_OVERFLOW,
+} rx_state_t;
 
-//Functions signature definition
-mp_obj_t machine_hw_can_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args);
-STATIC mp_obj_t machine_hw_can_init_helper(const machine_can_obj_t *self, size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args);
-STATIC mp_obj_t machine_hw_can_init(size_t n_args, const mp_obj_t *args, mp_map_t *kw_args);
-STATIC mp_obj_t machine_hw_can_deinit(const mp_obj_t self_in);
-STATIC void machine_hw_can_print(const mp_print_t *print, mp_obj_t self_in, mp_print_kind_t kind);
+extern const mp_obj_type_t esp32_can_type;
 
-STATIC mp_obj_t machine_hw_can_recv(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args);
-STATIC mp_obj_t machine_hw_can_send(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args);
-STATIC mp_obj_t machine_hw_can_get_tec(mp_obj_t self_in);
-STATIC mp_obj_t machine_hw_can_get_rec(mp_obj_t self_in);
-STATIC mp_obj_t machine_hw_can_get_tx_waiting_messages(mp_obj_t self_in);
-STATIC mp_obj_t machine_hw_can_get_rx_waiting_messages(mp_obj_t self_in);
-STATIC mp_obj_t machine_hw_can_get_state(mp_obj_t self_in);
-STATIC mp_obj_t machine_hw_can_clear_tx_queue(mp_obj_t self_in);
-STATIC mp_obj_t machine_hw_can_clear_rx_queue(mp_obj_t self_in);
-
-//Python function declarations
-MP_DEFINE_CONST_FUN_OBJ_1(machine_hw_can_deinit_obj, machine_hw_can_deinit);
-MP_DEFINE_CONST_FUN_OBJ_KW(machine_hw_can_init_obj, 4, machine_hw_can_init);
-MP_DEFINE_CONST_FUN_OBJ_KW(machine_hw_can_send_obj, 3, machine_hw_can_send);
-MP_DEFINE_CONST_FUN_OBJ_KW(machine_hw_can_recv_obj, 0, machine_hw_can_recv);
-MP_DEFINE_CONST_FUN_OBJ_1(machine_hw_can_get_tec_obj, machine_hw_can_get_tec);
-MP_DEFINE_CONST_FUN_OBJ_1(machine_hw_can_get_rec_obj, machine_hw_can_get_rec);
-MP_DEFINE_CONST_FUN_OBJ_1(machine_hw_can_get_tx_waiting_messages_obj, machine_hw_can_get_tx_waiting_messages);
-MP_DEFINE_CONST_FUN_OBJ_1(machine_hw_can_get_rx_waiting_messages_obj, machine_hw_can_get_rx_waiting_messages);
-MP_DEFINE_CONST_FUN_OBJ_1(machine_hw_can_get_state_obj, machine_hw_can_get_state);
-MP_DEFINE_CONST_FUN_OBJ_1(machine_hw_can_clear_tx_queue_obj, machine_hw_can_clear_tx_queue);
-MP_DEFINE_CONST_FUN_OBJ_1(machine_hw_can_clear_rx_queue_obj, machine_hw_can_clear_rx_queue);
-
-//Python objects list declaration
-STATIC const mp_rom_map_elem_t machine_can_locals_dict_table[] = {
-    //CAN_ATTRIBUTES
-    { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_CAN) },
-    //CAN_FUNCTIONS
-    { MP_ROM_QSTR(MP_QSTR_init), MP_ROM_PTR(&machine_hw_can_init_obj) },
-    { MP_ROM_QSTR(MP_QSTR_deinit), MP_ROM_PTR(&machine_hw_can_deinit_obj) },
-    { MP_ROM_QSTR(MP_QSTR_send), MP_ROM_PTR(&machine_hw_can_send_obj)},
-    { MP_ROM_QSTR(MP_QSTR_recv), MP_ROM_PTR(&machine_hw_can_recv_obj)},
-    { MP_OBJ_NEW_QSTR(MP_QSTR_get_tec), MP_ROM_PTR(&machine_hw_can_get_tec_obj)},
-    { MP_OBJ_NEW_QSTR(MP_QSTR_get_rec), MP_ROM_PTR(&machine_hw_can_get_rec_obj)},
-    { MP_OBJ_NEW_QSTR(MP_QSTR_get_tx_waiting_messages), MP_ROM_PTR(&machine_hw_can_get_tx_waiting_messages_obj)},
-    { MP_OBJ_NEW_QSTR(MP_QSTR_get_rx_waiting_messages), MP_ROM_PTR(&machine_hw_can_get_rx_waiting_messages_obj)},
-    { MP_OBJ_NEW_QSTR(MP_QSTR_get_state), MP_ROM_PTR(&machine_hw_can_get_state_obj)},
-    { MP_OBJ_NEW_QSTR(MP_QSTR_clear_tx_queue), MP_ROM_PTR(&machine_hw_can_clear_tx_queue_obj)},
-    { MP_OBJ_NEW_QSTR(MP_QSTR_clear_tx_queue), MP_ROM_PTR(&machine_hw_can_clear_rx_queue_obj)},
-    //CAN_MODE
-    { MP_ROM_QSTR(MP_QSTR_MODE_NORMAL), MP_ROM_INT(CAN_MODE_NORMAL) },
-    { MP_ROM_QSTR(MP_QSTR_MODE_NO_ACK), MP_ROM_INT(CAN_MODE_NO_ACK) },
-    { MP_ROM_QSTR(MP_QSTR_MODE_LISTEN_ONLY), MP_ROM_INT(CAN_MODE_LISTEN_ONLY) },
-    //CAN_STATE
-    { MP_ROM_QSTR(MP_QSTR_STATE_STOPPED), MP_ROM_INT(CAN_STATE_STOPPED) },
-    { MP_ROM_QSTR(MP_QSTR_STATE_RUNNING), MP_ROM_INT(CAN_STATE_RUNNING) },
-    { MP_ROM_QSTR(MP_QSTR_STATE_BUS_OFF), MP_ROM_INT(CAN_STATE_BUS_OFF) },
-    { MP_ROM_QSTR(MP_QSTR_STATE_RECOVERING), MP_ROM_INT(CAN_STATE_RECOVERING) },
-    //CAN_BAUDRATE
-    { MP_ROM_QSTR(MP_QSTR_BAUDRATE_25k), MP_ROM_INT(CAN_BAUDRATE_25k) },
-    { MP_ROM_QSTR(MP_QSTR_BAUDRATE_50k), MP_ROM_INT(CAN_BAUDRATE_50k) },
-    { MP_ROM_QSTR(MP_QSTR_BAUDRATE_100k), MP_ROM_INT(CAN_BAUDRATE_100k) },
-    { MP_ROM_QSTR(MP_QSTR_BAUDRATE_125k), MP_ROM_INT(CAN_BAUDRATE_125k) },
-    { MP_ROM_QSTR(MP_QSTR_BAUDRATE_250k), MP_ROM_INT(CAN_BAUDRATE_250k) },
-    { MP_ROM_QSTR(MP_QSTR_BAUDRATE_500k), MP_ROM_INT(CAN_BAUDRATE_500k) },
-    { MP_ROM_QSTR(MP_QSTR_BAUDRATE_800k), MP_ROM_INT(CAN_BAUDRATE_800k) },
-    { MP_ROM_QSTR(MP_QSTR_BAUDRATE_1M),   MP_ROM_INT(CAN_BAUDRATE_1M) },
-};
-STATIC MP_DEFINE_CONST_DICT(machine_can_locals_dict, machine_can_locals_dict_table);
-
-//Python object definition
-const mp_obj_type_t machine_can_type = {
-    { &mp_type_type },
-    .name = MP_QSTR_CAN,
-    .print = machine_hw_can_print, // give it a print-function
-    .make_new = machine_hw_can_make_new,  // give it a constructor
-    .locals_dict = (mp_obj_dict_t*)&machine_can_locals_dict, // and the global members
-};
-
+#endif // MICROPY_HW_ENABLE_CAN
 #endif // MICROPY_INCLUDED_ESP32_CAN_H
