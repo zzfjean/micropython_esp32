5ee164e60dd124f01511f36cfe89469255fa18fc
zzfjean <47771147+zzfjean@users.noreply.github.com>
2023-07-18 15:23:44 +0800

    Update machine_can.c
    Signed-off-by: zzfjean <47771147+zzfjean@users.noreply.github.com>

--- ports/esp32/machine_can.c
+++ ports/esp32/machine_can.c
@@ -795,10 +795,12 @@ MP_DEFINE_CONST_OBJ_TYPE(
     machine_can_type,
     MP_QSTR_CAN,
     MP_TYPE_FLAG_NONE,
-    make_new, esp32_hw_can_make_new,
-    print, esp32_hw_can_print,
-    locals_dict, (mp_obj_dict_t *)&esp32_can_locals_dict
-    );
+    make_new, machine_hw_can_make_new,
+    print, machine_hw_can_print,
+    locals_dict, (mp_obj_dict_t *)&machine_can_locals_dict
+);
+
+
 /*
 const mp_obj_type_t machine_can_type = {
     {&mp_type_type},
